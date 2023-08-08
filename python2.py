534321e90c235e7fd797529606838a5366c5a61c
srividya bellamkonda <140394916+srividya9642@users.noreply.github.com>
2023-08-08 11:13:43 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,29 @@
+#list   objects
+enames=["rahul","sonia","priyanka"]
+
+for ename in enames:
+      print(ename)
+i=0
+while i<len(enames):
+    print(enames[i])
+    i=i+1
+  
+ #tuple objects
+sizes=(38,40,42,44)
+for size in sizes:
+      print (sizes)
+      
+i=0
+while i<len(sizes):
+    print(sizes[i])
+    i=i+1
+    
+#srt objects
+ids={102,102,103,101,104}
+for id in ids:
+    print(ids)
+    
+i=0
+while i<len(ids):
+      print(ids[i])
+      i=i+1
